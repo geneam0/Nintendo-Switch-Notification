d8e3baf77a08c1609daa1af004fa80fcb58a0c34
Gene Lam <gene.lam@macaulay.cuny.edu>
2020-07-08 19:42:04 -0400

    fixed debug feature and global var

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 #from selenium.webdriver.chrome.options import Options
 
-phone_number=
-PATH = 
-Out_of_Stock=True
+phone_number= 
+PATH =
 
 def getStock(store,url):
     #chrome_options = Options()
@@ -40,11 +39,12 @@ def getStock(store,url):
         driver.quit()
 
 def main():
+    Out_of_Stock = True
     while Out_of_Stock:
         gameStop_hasGame=getStock("GameStop",'https://www.gamestop.com/video-games/switch/consoles/products/nintendo-switch-with-neon-blue-and-neon-red-joy-con/11095819.html?rt=productDetailsRedesign&utm_expid=.Hn2ODSotSjW4yWszOrBF7Q.1&utm_referrer=https%3A%2F%2Fwww.gamestop.com%2Fsearch%2F%3Fq%3Dnintendo%252Bswitch%26lang%3Ddefault%26rule%3Dbest-matches')
         target_hasGame=getStock("Target",'https://www.target.com/p/nintendo-switch-with-neon-blue-and-neon-red-joy-con/-/A-77464001?clkid=a786bdcdNbe8311ea81b742010a246e0e&lnm=81938&afid=NeuIntel%2C%20LLC&ref=tgt_adv_xasd0002')
         bestbuy_hasGame=getStock("BestBuy",'https://www.bestbuy.com/site/nintendo-switch-32gb-console-neon-red-neon-blue-joy-con/6364255.p?skuId=6364255')
-        if not gameStop_hasGame:
+        if gameStop_hasGame:
             os.system('osascript send.scpt {} "{}"'.format(phone_number, "Gamestop has the switch"))
             print("Gamestop has the switch")
             Out_of_Stock=False
